55ce5b5356c6074162430ddfbe03514bca9c04bd
Adam D <adamd@elementarydigital.co.uk>
2016-05-17 00:57:11 +0100

    updated sample site content

--- phpengine/PScript/PScript_Blocks.php
+++ phpengine/PScript/PScript_Blocks.php
@@ -2,6 +2,14 @@
 
 class PScript_Blocks {
 	
+	public $data = array();
+	
+	function data($data) {
+		
+		$this->data = array_merge($this->data, $data);
+		
+	}
+	
 	function get($block, $MyPhp=false) {
 		$Blocks = $this;
 		require("myapp/blocks/{$block}.phtml");
