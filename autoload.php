9325b203dc2dc7ea95ede1f69a2aa49c231f38d1
Adam D <adamd@elementarydigital.co.uk>
2016-05-21 14:46:52 +0100

    spl_load

--- autoload.php
+++ autoload.php
@@ -3,10 +3,11 @@ function __autoload($class_name)
 {
     $filename = $class_name.'.php';
 
-    $file = getcwd().'/phpengine/PScript/'.$filename;
+    $file = 'phpengine/PScript/'.$filename;
     if ( ! file_exists($file))
     {
         return FALSE;
     }
     require_once $file;
 }
+spl_autoload_register('__autoload');
