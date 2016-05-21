5835b7a005f4b2ba286dddc414acb58313bb818b
Adam D <adamd@elementarydigital.co.uk>
2016-05-21 14:34:07 +0100

    debugging autoloader

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@ function __autoload($class_name)
     $filename = $class_name.'.php';
 
     $file = getcwd().'/phpengine/PScript/'.$filename;
-
+				die(var_dump($file));
     if ( ! file_exists($file))
     {
         return FALSE;
