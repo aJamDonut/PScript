03983ffab777f5372ee6f20f4d69419434e73218
Adam D <adamd@elementarydigital.co.uk>
2016-05-21 14:29:57 +0100

    attempt 1. resolving travis integration issues with autoloader

--- autoload.php
+++ autoload.php
@@ -3,11 +3,11 @@ function __autoload($class_name)
 {
     $filename = $class_name.'.php';
 
-    $file = 'phpengine/PScript/'.$filename;
+    $file = getcwd().'/phpengine/PScript/'.$filename;
 
     if ( ! file_exists($file))
     {
         return FALSE;
     }
-    include $file;
+    require_once $file;
 }
